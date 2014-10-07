96a7d875e133d143f901fae93daefa66b1f25049
Fei Xia <feixia@cs.cmu.edu>
2014-10-07 16:06:19 -0400

    add sentence type

---
+++ hw2-feixia/src/main/java/edu/cmu/deiis/types/Sentence.java
@@ -0,0 +1,101 @@
+
+
+/* First created by JCasGen Tue Oct 07 15:52:34 EDT 2014 */
+package edu.cmu.deiis.types;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Tue Oct 07 15:55:11 EDT 2014
+ * XML source: /home/fei/Projects/java_workspace/CMU11791/hw2-feixia/src/main/resources/descriptors/collection_reader/FileSystemCollectionReader.xml
+ * @generated */
+public class Sentence extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Sentence.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Sentence() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public Sentence(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Sentence(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Sentence(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: identifier
+
+  /** getter for identifier - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getIdentifier() {
+    if (Sentence_Type.featOkTst && ((Sentence_Type)jcasType).casFeat_identifier == null)
+      jcasType.jcas.throwFeatMissing("identifier", "edu.cmu.deiis.types.Sentence");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Sentence_Type)jcasType).casFeatCode_identifier);}
+    
+  /** setter for identifier - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setIdentifier(String v) {
+    if (Sentence_Type.featOkTst && ((Sentence_Type)jcasType).casFeat_identifier == null)
+      jcasType.jcas.throwFeatMissing("identifier", "edu.cmu.deiis.types.Sentence");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Sentence_Type)jcasType).casFeatCode_identifier, v);}    
+  }
+
+    
